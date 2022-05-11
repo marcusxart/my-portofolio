2343af71c6035a8aee203f4c2fd18e65e45ede03
marcusxart <cemeji64@gmail.com>
2022-05-11 09:49:33 +0100

    restarting everything

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-import GlobalStyle from "./styles/GlobalStyled";
-import Landing from "./pages/Landing";
-
 function App() {
-  return (
-    <div className="App">
-      <GlobalStyle />
-      <Landing />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/styles/rootStyled.js
+++
@@ -1,1 +0,0 @@
-export const primaryColor = `#01c077`;
