d7c41a156139af8c8a1e94c8d41ac3c7c6430212
marcusxart <cemeji64@gmail.com>
2022-05-11 10:36:30 +0100

    add intro component

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./intro.css";
+
+const Intro = () => {
+  return (
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, My name is </h2>
+          <h1 className="i-name">Chinonso Emerenwa</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Frontend Developer</div>
+              <div className="i-title-item">Digital Artist</div>
+              <div className="i-title-item">Content Creator</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="i-right">Right</div>
+    </div>
+  );
+};
+
+export default Intro;
